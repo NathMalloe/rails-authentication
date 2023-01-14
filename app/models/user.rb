8a78e7c8ac1a068c966da41035f236b5bd68165a
Amine Berrada <amineberradasouni@gmail.com>
2023-01-14 17:02:39 +0100

    adding user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings, dependent: :destroy
+  has_many :rockets, dependent: :destroy
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :rating, :inclusion => { :in => 0..5 }, :presence => { :message => " must be within 0-5" }
+  validates :planet_race, inclusion: { in: %w(Martian Human Saturnian Plutonian Venusian Mercurian Moonian), message: "%{value} is not a valid race. Please choose a race between : Martian, Human, Saturnian, Plutonian, Venusian, Mercurian, Moonian."}
 end
