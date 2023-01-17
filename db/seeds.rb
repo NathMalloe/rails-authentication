8ed03f618c34447336a6b1fa3f106bcd938883bf
Amine Berrada <amineberradasouni@gmail.com>
2023-01-17 21:25:32 +0100

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Rocket.destroy_all
+User.destroy_all
+
+jeanpierre = User.create!({ first_name: "Jean Pierre", last_name: "Daverio", rating: 4.5, planet_race: "Human", email: "jpdaverio@gmail.com", password: "azerty" })
+amine = User.create!({ first_name: "Amine", last_name: "Berrada", rating: 3.6, planet_race: "Martian", email: "amine@gmail.com", password: "security" })
+Rocket.create!({ name: "Ariane1", destination: "Pluton", description: "Some description about Ariane", price_per_earthday: "1000$", rating: 4.7, user: jeanpierre })
+Rocket.create!({ name: "Ariane2", destination: "Mars", description: "Some description about Ariane", price_per_earthday: "2000$", rating: 4.7, user: jeanpierre })
+Rocket.create!({ name: "Ariane3", destination: "Venus", description: "Some description about Ariane", price_per_earthday: "3000$", rating: 4.7, user: jeanpierre })
+Rocket.create!({ name: "Ariane4", destination: "Earth", description: "Some description about Ariane", price_per_earthday: "4000$", rating: 4.7, user: amine })
+Rocket.create!({ name: "Ariane5", destination: "Saturn", description: "Some description about Ariane", price_per_earthday: "5000$", rating: 4.7, user: amine })
+Rocket.create!({ name: "Ariane6", destination: "Moon", description: "Some description about Ariane", price_per_earthday: "6000$", rating: 4.7, user: amine })
+
