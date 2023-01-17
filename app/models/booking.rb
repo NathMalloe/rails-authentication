7fd33ee209bc04241a4f848e2943d88b9c5531c6
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-01-17 19:11:27 +0100

    add bokking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :rocket
   belongs_to :user
+  validates :start_date, presence: true, comparison: { greater_than: Date.today }
+  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+  validates :price, presence: true
+  validates :status, presence: true, length: { minimum: 2 }
 end
