cead8e97c603a0d592521f44aa5d5ac6674ebe81
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-01-19 17:09:58 +0100

    Photos on home, sign_up sign_in views improvements, seed description of rockets

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :rocket
   belongs_to :user
+  has_many_attached :photos
   validates :start_date, presence: true, comparison: { greater_than: Date.today }
   validates :end_date, presence: true, comparison: { greater_than: :start_date }
   validates :price, presence: true
