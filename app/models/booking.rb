21c31a9c69b0b180e31279ebe48b10e1debfa3f1
NathMalloe <nathanael.lemoal@gmail.com>
2023-01-19 22:45:58 +0100

    ajout bookins

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,13 @@ class Booking < ApplicationRecord
   has_many_attached :photos
   validates :start_date, presence: true, comparison: { greater_than: Date.today }
   validates :end_date, presence: true, comparison: { greater_than: :start_date }
-  validates :price, presence: true
-  validates :status, presence: true, length: { minimum: 2 }
+  # validates :price, presence: true
+  # validates :status, presence: true, length: { minimum: 2 }
+
+  def total_price
+    # @rocket = Rocket.find(params[:rocket_id])
+    # @booking = Booking.find(params[:booking_id])
+    total_days = end_date - start_date + 1
+    total_days * rocket.price_per_earthday
+  end
 end
